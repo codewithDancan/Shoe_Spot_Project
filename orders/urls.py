be594438393bd3aa2cfa0d2983665e67f323ed7e
Dancan <ngagadancan2003@gmail.com>
2024-08-07 00:11:21 +0300

    Modifield urls

--- orders/urls.py
+++ orders/urls.py
@@ -1,12 +1,19 @@
 from django.urls import path
-from . import views
+from .views import (
+    order_list,
+    order_detail,
+    order_create, 
+    order_delete,
+    order_update,
+)
 
 app_name = "orders"
 
+
 urlpatterns = [
-    path('order/', views.order_list, name='order-list'),
-    path('order/<uuid:order_id>/', views.order_detail, name='order-detail'),
-    path('order/new/', views.order_create, name='order-create'),
-    path('order/<uuid:order_id>/edit/', views.order_update, name='order-update'),
-    path('order/<uuid:order_id>/delete/', views.order_delete, name='order-delete'),
+    path('order/', order_list, name='order-list'),
+    path('order/<uuid:order_id>/', order_detail, name='order-detail'),
+    path('order/new/', order_create, name='order-create'),
+    path('order/<uuid:order_id>/edit/', order_update, name='order-update'),
+    path('order/<uuid:order_id>/delete/', order_delete, name='order-delete'),
 ]
